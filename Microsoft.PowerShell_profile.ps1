6d61ab6f0644de0acbf6d7b8dbee0e2549292585
Daniel Liuzzi <dliuzzi@gmail.com>
2017-04-13 02:31:03 +0200

    Remove commented out code

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,42 +6,6 @@ Import-Module PSColor
 . (Resolve-Path "$env:github_posh_git\profile.example.ps1")
 
 $GitPromptSettings.EnableWindowTitle = ' '
-
-# # Useful, but too slow
-# $GitPromptSettings.EnableStashStatus = $true
-
-# $global:GitPromptSettings.BeforeText = '['
-# $global:GitPromptSettings.AfterText  = '] '
-
-# # Prompt
-# function global:prompt {
-#     $realLASTEXITCODE = $LASTEXITCODE
-
-#     $ui = $Host.UI.RawUI
-
-#     $path = $PWD.Path
-#     if ($path.StartsWith($env:HOME)) {
-#         $path = '~' + $path.Substring($env:HOME.Length)
-#     }
-#     elseif ($path.StartsWith($env:SystemDrive)) {
-#         $path = $path.Substring($env:SystemDrive.Length)
-#     }
-
-#     # Path
-#     $bg = $ui.BackgroundColor
-#     $ui.BackgroundColor = 'Blue'
-#     Write-Host $path.PadRight($ui.WindowSize.Width - 1, ' ')
-#     $ui.BackgroundColor = $bg
-
-#     # Reset color, which can be messed up by Enable-GitColors
-#     $ui.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
-
-#     Write-VcsStatus
-
-#     $global:LASTEXITCODE = $realLASTEXITCODE
-#     return '> '
-# }
-
 $GitPromptSettings.DefaultPromptSuffix = '
 > '
 
@@ -52,9 +16,6 @@ Set-Alias po popd
 Set-Alias pu pushd
 
 # Utilities
-# # function atom { cmd /c "$env:LOCALAPPDATA\atom\Update.exe --processStart atom.exe" } # http://stackoverflow.com/a/4167071
-# # Set-Alias code "${env:ProgramFiles(x86)}\Microsoft VS Code\Code.exe"
-# Set-Alias subl "$env:ProgramFiles\Sublime Text 3\subl.exe" # http://stackoverflow.com/a/10853587
 Set-Alias wm winmerge
 Set-Alias edit subl
 Set-Alias e edit
@@ -62,12 +23,7 @@ Set-Alias e edit
 # Git
 function a { git add @args }
 function b { git branch @args }
-
-# # -vv doesn't play well on Sublime Text w/editorconfig-sublime
-# # https://github.com/sindresorhus/editorconfig-sublime/issues/55
-# function co { git commit -vv @args }
 function c { git commit @args }
-
 function co { git checkout @args }
 function d { git diff @args }
 function ds { git diff --staged @args }
@@ -112,8 +68,6 @@ function .......... { cd ..\..\..\..\..\..\..\..\.. }
 function ........... { cd ..\..\..\..\..\..\..\..\..\.. }
 
 # Miscellaneous
-# function e { if ($args) { subl @args } else { subl . } }
-# # function e { if ($args) { atom @args } else { atom . } }
 function la { ls -force @args }
 function mcd { mkdir @args >$null; cd @args }
 function o { if ($args) { start @args } else { start . } }
