b215062f0542b49903a09319a95f2828f9fcd642
Daniel Liuzzi <dliuzzi@gmail.com>
2020-04-25 21:05:53 +0200

    VS Code as default editor

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -136,6 +136,7 @@ function open { if ($args) { start @args } else { start . } }
 
 # Make git diff --stat show full file path - https://stackoverflow.com/a/16733338
 $env:COLUMNS = 170
+$env:EDITOR = 'code --wait' # VS Code as default editor - https://stackoverflow.com/a/57144660/88709
 
 # Add bin/ to the system path
 $env:path += ";$env:USERPROFILE\bin\"
