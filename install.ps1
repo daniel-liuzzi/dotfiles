ed39ba81da2af428b4d814637465fd5ef7343b36
Daniel Liuzzi <dliuzzi@gmail.com>
2020-06-19 21:45:41 +0200

    Bye bye, Sublime Text :(

--- install.ps1
+++ install.ps1
@@ -30,7 +30,6 @@ New-Link ".\AutoHotkeyU64.custom.ahk" "~\Documents\AutoHotkeyU64.custom.ahk"
 New-Link ".\Microsoft.PowerShell_profile.ps1" $PROFILE.CurrentUserCurrentHost
 New-Link ".\Microsoft.PowerShell_profile.custom.ps1" ($PROFILE.CurrentUserCurrentHost -replace '.ps1', '.custom.ps1')
 New-Link ".\bin\" "~\bin\"
-New-Link ".\Preferences.sublime-settings" "~\scoop\persist\sublime-text\Data\Packages\User\Preferences.sublime-settings"
 New-Link ".\profiles.json" "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\profiles.json"
 New-Link ".\vscode.json" "$env:APPDATA\Code\User\settings.json"
 
