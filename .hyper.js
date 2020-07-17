a88a384abaccdefa10eadd285b88509f9c4e6ba9
Daniel Liuzzi <dliuzzi@gmail.com>
2020-07-17 20:15:55 +0200

    Switch from Victor Mono back to Cascadia Code
    Victor Mono breaks `console.table()` format in VS Code.

--- .hyper.js
+++ .hyper.js
@@ -12,7 +12,7 @@ module.exports = {
     fontSize: 17,
 
     // font family with optional fallbacks
-    fontFamily: '"Victor Mono", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: '"Cascadia Code PL", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
