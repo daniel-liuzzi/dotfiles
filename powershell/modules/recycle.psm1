08e41a73ae829cc1176cc55aa978a85b1bbfa75c
Daniel Liuzzi <dliuzzi@gmail.com>
2021-10-21 21:19:14 +0200

    PS: cosmetics

--- powershell/modules/recycle.psm1
+++ powershell/modules/recycle.psm1
@@ -1,8 +1,8 @@
-Import-Module Recycle
-
-Set-Alias -Name 'del' -Value 'Remove-ItemSafely' -Option AllScope -Scope Global
-Set-Alias -Name 'erase' -Value 'Remove-ItemSafely' -Option AllScope -Scope Global
-Set-Alias -Name 'rd' -Value 'Remove-ItemSafely' -Option AllScope -Scope Global
-Set-Alias -Name 'ri' -Value 'Remove-ItemSafely' -Option AllScope -Force -Scope Global
-Set-Alias -Name 'rm' -Value 'Remove-ItemSafely' -Option AllScope -Scope Global
-Set-Alias -Name 'rmdir' -Value 'Remove-ItemSafely' -Option AllScope -Scope Global
+Get-Alias -Definition 'Remove-Item' | ForEach-Object {
+    Set-Alias `
+        -Name $_.Name `
+        -Value 'Remove-ItemSafely' `
+        -Option AllScope `
+        -Scope Global `
+        -Force
+}
