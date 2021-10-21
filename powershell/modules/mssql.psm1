e0f0c907b2b00a32f8726aca169bd5236728c0d5
Daniel Liuzzi <dliuzzi@gmail.com>
2021-10-21 11:52:24 +0200

    PS: cosmetics

--- powershell/modules/mssql.psm1
+++ powershell/modules/mssql.psm1
@@ -1,11 +1,22 @@
 function Stop-SqlServer {
-    sudo pwsh -Command 'net stop SQLSERVERAGENT; net stop MSSQLSERVER'
+    sudo Invoke-Expression '
+        net stop SQLSERVERAGENT
+        net stop MSSQLSERVER
+    '
 }
 
 function Start-SqlServer {
-    sudo pwsh -Command 'net start MSSQLSERVER; net start SQLSERVERAGENT'
+    sudo Invoke-Expression '
+        net start MSSQLSERVER
+        net start SQLSERVERAGENT
+    '
 }
 
 function Restart-SqlServer {
-    sudo pwsh -Command 'Stop-SqlServer; Start-SqlServer'
+    sudo Invoke-Expression '
+        net stop SQLSERVERAGENT
+        net stop MSSQLSERVER
+        net start MSSQLSERVER
+        net start SQLSERVERAGENT
+    '
 }
