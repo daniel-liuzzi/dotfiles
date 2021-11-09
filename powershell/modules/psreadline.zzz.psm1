7d05791058a80186bdf646879379f3a727f27f77
Daniel Liuzzi <dliuzzi@gmail.com>
2021-11-09 09:08:21 +0100

    PS: PowerShell 7.2
    PredictionViewStyle doesn't yet work in PowerShell 7.2
    https://github.com/PowerShell/PSReadLine/issues/2957#issuecomment-963610079

--- powershell/modules/psreadline.zzz.psm1
+++ powershell/modules/psreadline.zzz.psm1
@@ -4,5 +4,5 @@ Set-PSReadLineKeyHandler -Key 'DownArrow' -Function 'NextHistory'
 Set-PSReadLineKeyHandler -Key 'Tab', 'Shift+Tab' -Function 'MenuComplete'
 Set-PSReadLineOption -EditMode Windows
 Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView
+# Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -WordDelimiters ' '
