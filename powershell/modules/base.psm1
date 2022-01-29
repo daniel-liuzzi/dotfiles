07e9cb45dc914f357ab488ab34d997655daff462
Daniel Liuzzi <dliuzzi@gmail.com>
2022-01-29 03:49:10 +0100

    PS: Improve run function
    It no longer chokes on quoted arguments, i.e., `lg -G"hello world"`

--- powershell/modules/base.psm1
+++ powershell/modules/base.psm1
@@ -54,15 +54,19 @@ function Format-RelativeDate ([datetime]$Value) {
 }
 
 function run {
-    $Command = $args.ForEach( {
-            if ($_ -isnot [string]) { return $_ }
-            $Qualify = $_.ToCharArray().ForEach( { [char]::IsWhiteSpace($_) -or $_ -in @('"', "'") } ).Contains($true)
-            $Value = $_.Replace("'", "''")
-            if ($Qualify) { "'$Value'" } else { $Value }
-        } ) -join ' '
+    if (!$Quiet) {
+        $CommandLine = $args.ForEach({
+                if ($_ -isnot [string]) { return $_ }
+                $Qualify = $_.ToCharArray().ForEach( { [char]::IsWhiteSpace($_) -or $_ -in @('"', "'") } ).Contains($true)
+                $Value = $_.Replace("'", "''")
+                if ($Qualify) { "'$Value'" } else { $Value }
+            }) -join ' '
 
-    if (!$Quiet) { Write-Host "> $Command" -ForegroundColor DarkGray }
-    Invoke-Expression $Command
+        Write-Host "> $CommandLine" -ForegroundColor DarkGray
+    }
+
+    $Command, $Arguments = $args
+    & $Command $Arguments
 }
 
 function quietly { $Quiet = $true; run @args }
