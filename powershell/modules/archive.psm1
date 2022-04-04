05a818df46190a0f84c30774dcceb0a17a718056
Daniel Liuzzi <dliuzzi@gmail.com>
2022-04-04 22:47:14 +0200

    PS: rename ~/!Archive to ~/.archive

--- powershell/modules/archive.psm1
+++ powershell/modules/archive.psm1
@@ -1,7 +1,7 @@
 function archive {
     # TODO: delete node_modules, bin, obj recursively
     $Date = Get-Date -Format 'yyyy-MM-dd'
-    $Path = Join-Path '~/!Archive' $Date
+    $Path = Join-Path '~/.archive' $Date
     New-Item -Path $Path -ItemType Directory -Force | Out-Null
     Get-ChildItem -Path '~/Desktop' -Recurse -Force | Move-Item -Destination $Path | Out-Null
     Write-Output 'Desktop archived successfully.'
