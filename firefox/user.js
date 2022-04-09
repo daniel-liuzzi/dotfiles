4a95b6279ac94ac608b0f427f27bee7cb1d43469
Daniel Liuzzi <dliuzzi@gmail.com>
2022-04-09 13:33:17 +0200

    Firefox: add some user preferences

--- firefox/user.js
+++ firefox/user.js
@@ -1,3 +1,16 @@
 // Set Firefox to look for userChrome.css at startup
 // https://www.userchrome.org/how-create-userchrome-css.html#aboutconfig
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Ctrl+Tab cycles through tabs in recently used order
+user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
+
+// Bookmarks Toolbar > Never Show
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+
+// Don't warn when accessing about:config
+user_pref("browser.aboutConfig.showWarning", false);
+
+// DevTools
+user_pref("devtools.everOpened", true);
+user_pref("devtools.theme.show-auto-theme-info", false);
