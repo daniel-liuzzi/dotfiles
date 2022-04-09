0956c2625f1a7e8c9d36b907a53b7aef079dcf3b
Daniel Liuzzi <dliuzzi@gmail.com>
2022-04-09 12:51:25 +0200

    Firefox: simplify installation

---
+++ firefox/user.js
@@ -0,0 +1,3 @@
+// Set Firefox to look for userChrome.css at startup
+// https://www.userchrome.org/how-create-userchrome-css.html#aboutconfig
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
