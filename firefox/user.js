cc35250bcb257fbb192b25299eb0606b7c36b49f
Daniel Liuzzi <dliuzzi@gmail.com>
2022-04-11 23:03:30 +0200

    FF: don't remember search and form history

--- firefox/user.js
+++ firefox/user.js
@@ -11,6 +11,9 @@ user_pref("browser.toolbars.bookmarks.visibility", "never");
 // Don't warn when accessing about:config
 user_pref("browser.aboutConfig.showWarning", false);
 
+// Don't remember search and form history
+user_pref("browser.formfill.enable", false);
+
 // DevTools
 user_pref("devtools.everOpened", true);
 user_pref("devtools.theme.show-auto-theme-info", false);
