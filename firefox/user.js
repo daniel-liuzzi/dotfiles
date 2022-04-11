799cddb3302a0df7017eb39ba0669c425c27191d
Daniel Liuzzi <dliuzzi@gmail.com>
2022-04-11 23:06:46 +0200

    FF: alphabetize user_pref keys

--- firefox/user.js
+++ firefox/user.js
@@ -1,19 +1,19 @@
-// Set Firefox to look for userChrome.css at startup
-// https://www.userchrome.org/how-create-userchrome-css.html#aboutconfig
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+// Don't warn when accessing about:config
+user_pref("browser.aboutConfig.showWarning", false);
 
 // Ctrl+Tab cycles through tabs in recently used order
 user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
 
-// Bookmarks Toolbar > Never Show
-user_pref("browser.toolbars.bookmarks.visibility", "never");
-
-// Don't warn when accessing about:config
-user_pref("browser.aboutConfig.showWarning", false);
-
 // Don't remember search and form history
 user_pref("browser.formfill.enable", false);
+
+// Bookmarks Toolbar > Never Show
+user_pref("browser.toolbars.bookmarks.visibility", "never");
 
 // DevTools
 user_pref("devtools.everOpened", true);
 user_pref("devtools.theme.show-auto-theme-info", false);
+
+// Set Firefox to look for userChrome.css at startup
+// https://www.userchrome.org/how-create-userchrome-css.html#aboutconfig
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
