33a531826d2f69be3bd3e70381197735d2b14655
Daniel Liuzzi <dliuzzi@gmail.com>
2022-05-06 11:43:45 +0200

    oh-my-post from scoop

--- powershell/modules/prompt.psm1
+++ powershell/modules/prompt.psm1
@@ -1,6 +1,6 @@
 # Oh my Posh
 $env:POSH_SESSION_DEFAULT_USER = $env:USERNAME # Hide default user@host
-Set-PoshPrompt -Theme "$Root/oh-my-posh/daniel.omp.json"
+oh-my-posh init pwsh --config "$Root/oh-my-posh/daniel.omp.json" | Invoke-Expression
 
 # Workaround for weird bug when using -NoLogo
 # https://github.com/microsoft/terminal/issues/8341#issuecomment-917650188
