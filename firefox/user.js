c947c9a19fd2fdbb3f69573e7c766c8f4c2a41ba
Daniel Liuzzi <dliuzzi@gmail.com>
2022-06-06 03:22:37 +0200

    FF: enable devtools.chrome.enabled

--- firefox/user.js
+++ firefox/user.js
@@ -10,6 +10,12 @@ user_pref("browser.formfill.enable", false);
 // Bookmarks Toolbar > Never Show
 user_pref("browser.toolbars.bookmarks.visibility", "never");
 
+// The Browser Console command line is disabled by default. To enable it set the
+// devtools.chrome.enabled preference to true in about:config, or set the
+// “Enable chrome debugging” option in the developer tool settings.
+// https://firefox-source-docs.mozilla.org/devtools-user/browser_console/index.html#browser-console-command-line
+user_pref("devtools.chrome.enabled", true);
+
 // DevTools
 user_pref("devtools.everOpened", true);
 user_pref("devtools.theme.show-auto-theme-info", false);
