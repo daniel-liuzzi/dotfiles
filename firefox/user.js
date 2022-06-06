f7c67616bf4bb9d2998e0be79b8f7a01a935e6b7
Daniel Liuzzi <dliuzzi@gmail.com>
2022-06-06 14:17:11 +0200

    FF: DevTools settings

--- firefox/user.js
+++ firefox/user.js
@@ -10,15 +10,21 @@ user_pref("browser.formfill.enable", false);
 // Bookmarks Toolbar > Never Show
 user_pref("browser.toolbars.bookmarks.visibility", "never");
 
-// The Browser Console command line is disabled by default. To enable it set the
-// devtools.chrome.enabled preference to true in about:config, or set the
-// “Enable chrome debugging” option in the developer tool settings.
-// https://firefox-source-docs.mozilla.org/devtools-user/browser_console/index.html#browser-console-command-line
-user_pref("devtools.chrome.enabled", true);
-
 // DevTools
+user_pref("devtools.browserconsole.contentMessages", true);
+user_pref("devtools.browserconsole.filter.css", true);
+user_pref("devtools.browserconsole.filter.net", true);
+user_pref("devtools.browserconsole.filter.netxhr", true);
+user_pref("devtools.cache.disabled", true);
+user_pref("devtools.chrome.enabled", true);
 user_pref("devtools.everOpened", true);
+user_pref("devtools.netmonitor.persistlog", true);
 user_pref("devtools.theme.show-auto-theme-info", false);
+user_pref("devtools.webconsole.filter.css", true);
+user_pref("devtools.webconsole.filter.net", true);
+user_pref("devtools.webconsole.filter.netxhr", true);
+user_pref("devtools.webconsole.persistlog", true);
+user_pref("devtools.webconsole.timestampMessages", true);
 
 // Set Firefox to look for userChrome.css at startup
 // https://www.userchrome.org/how-create-userchrome-css.html#aboutconfig
