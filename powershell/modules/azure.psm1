1d4b087d77d32eed54a41b34c3f12b3f4c93e6db
Daniel Liuzzi <dliuzzi@gmail.com>
2022-06-22 00:04:30 +0200

    PS: don't colorize az when piping output

--- powershell/modules/azure.psm1
+++ powershell/modules/azure.psm1
@@ -1,5 +1,9 @@
 $AzureCli = Get-Command 'az'
 
-function Invoke-AzureCli { & $AzureCli @args | jq -C }
+function Invoke-AzureCli {
+    & $AzureCli @args | jq @(
+        if ($MyInvocation.PipelinePosition -eq $MyInvocation.PipelineLength) { '-C' }
+    )
+}
 
 Set-Alias -Name 'az' -Value 'Invoke-AzureCli'
