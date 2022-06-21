4e75fc68032801c04d92b0c39af75d349093fbc0
Daniel Liuzzi <dliuzzi@gmail.com>
2022-06-21 21:44:34 +0200

    PS: which cmdlet

--- powershell/modules/misc.psm1
+++ powershell/modules/misc.psm1
@@ -7,6 +7,7 @@ function hosts { sudo code $env:SystemRoot\System32\drivers\etc\hosts }
 function la { Get-ChildItem -Force @args }
 function mcd { mkdir @args | Set-Location }
 function sh { & '~/scoop/apps/git/current/bin/sh.exe' @args }
+function which { Get-Command @args | Select-Object -ExpandProperty Definition }
 
 Set-Alias -Name 'ads' -Value 'azuredatastudio'
 Set-Alias -Name 'l' -Value 'Get-ChildItem'
