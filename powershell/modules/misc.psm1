08d01fce3d01b025457267200919b4327c9db1db
Daniel Liuzzi <dliuzzi@gmail.com>
2022-06-22 11:52:51 +0200

    PS: Get-SavedHistory
    Credit to https://stackoverflow.com/a/44104044/88709

--- powershell/modules/misc.psm1
+++ powershell/modules/misc.psm1
@@ -3,6 +3,7 @@ function \ { Set-Location '\' }
 function ~ { Set-Location '~' }
 function .. { Set-Location '..' }
 
+function Get-SavedHistory { Get-Content (Get-PSReadlineOption).HistorySavePath @args }
 function hosts { sudo code $env:SystemRoot\System32\drivers\etc\hosts }
 function la { Get-ChildItem -Force @args }
 function mcd { mkdir @args | Set-Location }
@@ -10,6 +11,7 @@ function sh { & '~/scoop/apps/git/current/bin/sh.exe' @args }
 function which { Get-Command @args | Select-Object -ExpandProperty Definition }
 
 Set-Alias -Name 'ads' -Value 'azuredatastudio'
+Set-Alias -Name 'hist' -Value 'Get-SavedHistory'
 Set-Alias -Name 'l' -Value 'Get-ChildItem'
 Set-Alias -Name 'o' -Value 'Start-Process'
 Set-Alias -Name 'open' -Value 'Start-Process'
