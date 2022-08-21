6b267b3aaa7924d81b732e5caee72635cd6bd1ea
Daniel Liuzzi <dliuzzi@gmail.com>
2022-08-21 18:47:15 +0200

    Console title improvements #omp
    - Replace folder name with repo name
    - Replace "root" with thunderbolt emoji

--- powershell/modules/prompt.psm1
+++ powershell/modules/prompt.psm1
@@ -1,5 +1,12 @@
 oh-my-posh init pwsh --config "$Root/oh-my-posh/daniel.omp.json" | Invoke-Expression
 
+function Set-EnvironmentVariables {
+    $RepoPath = git rev-parse --show-toplevel
+    $env:REPO_NAME = if ($RepoPath) { [System.IO.Path]::GetFileName($RepoPath) }
+}
+
+New-Alias -Name Set-PoshContext -Value Set-EnvironmentVariables -Scope Global -Force
+
 # Workaround for weird bug when using -NoLogo
 # https://github.com/microsoft/terminal/issues/8341#issuecomment-917650188
 Clear-Host
