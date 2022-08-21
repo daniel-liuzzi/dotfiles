70eb545604a79e76ba7681051ddf8671341493bf
Daniel Liuzzi <dliuzzi@gmail.com>
2022-08-21 21:09:36 +0200

    Add HEAD rev to prompt #git

--- powershell/modules/prompt.psm1
+++ powershell/modules/prompt.psm1
@@ -3,6 +3,7 @@ oh-my-posh init pwsh --config "$Root/oh-my-posh/daniel.omp.json" | Invoke-Expres
 function Set-EnvironmentVariables {
     $RepoPath = git rev-parse --show-toplevel
     $env:REPO_NAME = if ($RepoPath) { [System.IO.Path]::GetFileName($RepoPath) }
+    $env:HEAD_REV = git rev-parse --short HEAD
 }
 
 New-Alias -Name Set-PoshContext -Value Set-EnvironmentVariables -Scope Global -Force
