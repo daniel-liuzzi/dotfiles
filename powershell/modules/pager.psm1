347a96809292aaab7520eabdc2a2b0e5e11b5a79
Daniel Liuzzi <dliuzzi@gmail.com>
2022-08-28 15:55:24 +0200

    Pager improvements #terminal
    - Option arrays for clarity
    - Enable all available bat components
    - Replace line wrap with 4-char horizontal scroll
    - Ignore case in all searches
    - Don't quiet the terminal bell (screen flashing is even worse)
    - Don't display tildes after end of file

--- powershell/modules/pager.psm1
+++ powershell/modules/pager.psm1
@@ -4,9 +4,21 @@ function Invoke-Bat { run bat @args }
 
 Set-Alias -Name cat -Value Invoke-Bat -Option AllScope -Scope Global
 
-# By default Git sets --no-init which spams stdout, so we unset it
+$env:BAT_OPTS = @(
+    '--style=full'
+    '--wrap=never'
+)
+
 # https://git-scm.com/docs/git-config/2.37.2#Documentation/git-config.txt-corepager
-$env:LESS = '--quiet --quit-if-one-screen --raw-control-chars'
+$env:LESS = @(
+    '--chop-long-lines'
+    '--IGNORE-CASE'
+    # '--no-init' # spams stdout
+    '--quit-if-one-screen'
+    # '--raw-control-chars' # breaks --chop-long-lines
+    '--shift=4'
+    '--tilde'
+)
 
 # Better pager for commands that respect it, i.e., `help`
 # https://github.com/PowerShell/PowerShell/blob/158d3a64e9c6edabb77070074686b8a75027a8a5/src/System.Management.Automation/engine/InitialSessionState.cs#L4173
