244f58c40f7b0e1228a531d105e215b2c6e85613
Daniel Liuzzi <dliuzzi@gmail.com>
2022-09-25 18:05:25 +0200

    Cosmetics

--- powershell/modules/sql.psm1
+++ powershell/modules/sql.psm1
@@ -1,9 +1,9 @@
+using namespace System.Web
+
 # PS 5.1 needs this - https://stackoverflow.com/a/38409183/88709
 Add-Type -AssemblyName System.Web
 
-function Get-UrlEncoded ($s) {
-    [System.Web.HTTPUtility]::UrlEncode($s).Replace('+', '%20')
-}
+function Get-UrlEncoded ($s) { [HTTPUtility]::UrlEncode($s).Replace('+', '%20') }
 
 if ($global:SqlOptions.Databases) {
     foreach ($db in $global:SqlOptions.Databases.GetEnumerator()) {
