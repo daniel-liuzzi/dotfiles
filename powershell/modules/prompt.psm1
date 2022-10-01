5942ba86d865c53ae854f5f33124ec1def6ccf30
Daniel Liuzzi <dliuzzi@gmail.com>
2022-10-01 19:01:02 +0200

    Prompt improvements #omp
    - Use built-in RepoName property
    - Array-based template

--- powershell/modules/prompt.psm1
+++ powershell/modules/prompt.psm1
@@ -1,8 +1,6 @@
 oh-my-posh init pwsh --config "$Root/oh-my-posh/daniel.omp.json" | Invoke-Expression
 
 function Set-EnvironmentVariables {
-    $RepoPath = git rev-parse --show-toplevel
-    $env:REPO_NAME = if ($RepoPath) { [System.IO.Path]::GetFileName($RepoPath) }
     $env:HEAD_REV = git rev-parse --short HEAD
 }
 
