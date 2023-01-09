61531a63856aa233d4808f0b0251f630945f7d23
Daniel Liuzzi <dliuzzi@gmail.com>
2023-01-09 18:38:28 +0100

    Update to v3.3 #jrnl

--- powershell/modules/jrnl.psm1
+++ powershell/modules/jrnl.psm1
@@ -6,10 +6,10 @@ Import-Module $ProfileDir/modules/base
 #>
 function Get-DailyJournal {
     function Display ($Range) {
-        $Entry = Invoke-Expression "jrnl $Range --format dates" | Select-Object -First 1
-        if ($Entry -notmatch '^\d{4}-\d{2}-\d{2}') { return }
+        $Entry = Invoke-Expression "jrnl $Range --format dates" 2> $null | Select-Object -First 1
+        if ($null -eq $Entry -or $Entry -notmatch '^\d{4}-\d{2}-\d{2}') { return }
         Write-Host "`n$(Format-RelativeDate $matches.0)`n" -ForegroundColor Blue
-        quietly j -on ('{0:yyyy-MM-dd}' -f $matches.0) $JournalArgs
+        quietly j -on ('{0:yyyy-MM-dd}' -f $matches.0) $JournalArgs 2> $null
     }
 
     $JournalArgs = $args
@@ -18,10 +18,7 @@ function Get-DailyJournal {
     Display ('-from {0:yyyy-MM-dd}' -f [datetime]::Today.AddDays(1))
 }
 
-function Get-WeeklyJournal { j -from monday -to today --format short @args }
-
-# jrnl v2.8.1 doesn't work with Python 3.10
-function Repair-Journal { scoop reset python@3.9.7 }
+function Get-WeeklyJournal { j -from monday -to today --format short @args 2> $null }
 
 function j { run jrnl @args }
 
