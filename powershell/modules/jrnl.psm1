2b196ee30dd307cc5aaba9b0757e6147d056d47a
Daniel Liuzzi <dliuzzi@gmail.com>
2023-01-09 20:02:39 +0100

    Simplify, cosmetics #jrnl

--- powershell/modules/jrnl.psm1
+++ powershell/modules/jrnl.psm1
@@ -1,21 +1,22 @@
 Import-Module $ProfileDir/modules/base
 
 <#
-  .SYNOPSIS
-  Get journal entries for the previuos, current, and next day
+    .SYNOPSIS
+    Get journal entries for the previuos, current, and next day
 #>
-function Get-DailyJournal {
-    function Display ($Range) {
-        $Entry = Invoke-Expression "jrnl $Range --format dates" 2> $null | Select-Object -First 1
+function Get-DailyJournal([datetime]$Date = [datetime]::Today) {
+    function Display($Op, $Delta, $Limit) {
+        $Day = '{0:yyyy-MM-dd}' -f $Date.AddDays($Delta)
+        $Entry = quietly j $Op $Day $Limit --format dates 2> $null | Select-Object -First 1
         if ($null -eq $Entry -or $Entry -notmatch '^\d{4}-\d{2}-\d{2}') { return }
         Write-Host "`n$(Format-RelativeDate $matches.0)`n" -ForegroundColor Blue
         quietly j -on ('{0:yyyy-MM-dd}' -f $matches.0) $JournalArgs 2> $null
     }
 
     $JournalArgs = $args
-    Display ('-to {0:yyyy-MM-dd} -n 1' -f [datetime]::Today.AddDays(-1))
-    Display ('-on {0:yyyy-MM-dd} -n 1' -f [datetime]::Today)
-    Display ('-from {0:yyyy-MM-dd}' -f [datetime]::Today.AddDays(1))
+    Display '-to' -1 '-n 1'
+    Display '-on' 0 '-n 1'
+    Display '-from' 1
 }
 
 function Get-WeeklyJournal { j -from monday -to today --format short @args 2> $null }
