a73003aff17631db7fb8d87cdf2b38b39c8d5450
Daniel Liuzzi <dliuzzi@gmail.com>
2023-03-31 19:17:02 +0200

    Docker whaler, dive #ps

---
+++ powershell/modules/docker.psm1
@@ -0,0 +1,12 @@
+# Credit: https://stackoverflow.com/a/30793515/88709
+
+function Invoke-Whaler {
+    run docker run --rm -v /var/run/docker.sock:/var/run/docker.sock alpine/dfimage $args
+}
+
+function Invoke-Dive {
+    run docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock wagoodman/dive $args
+}
+
+Set-Alias -Name 'whaler' -Value 'Invoke-Whaler'
+Set-Alias -Name 'dive' -Value 'Invoke-Dive'
