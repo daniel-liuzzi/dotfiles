713625fb43d7d79641fc2bbdd61e7ba217e78e4b
Daniel Liuzzi <dliuzzi@gmail.com>
2023-04-07 17:49:55 +0200

    DRY #ps

--- powershell/modules/psreadline.options.zzz.psm1
+++ powershell/modules/psreadline.options.zzz.psm1
@@ -2,10 +2,13 @@ if ($PSVersionTable.PSEdition -eq 'Core') {
     Import-Module Az.Tools.Predictor -Global
 }
 
-# psreadline overrides
-Set-PSReadLineKeyHandler -Key 'UpArrow' -Function 'PreviousHistory'
-Set-PSReadLineKeyHandler -Key 'DownArrow' -Function 'NextHistory'
-Set-PSReadLineKeyHandler -Key 'Tab', 'Shift+Tab' -Function 'MenuComplete'
-Set-PSReadLineOption -EditMode Windows
-Set-PSReadLineOption -PredictionViewStyle ListView
-Set-PSReadLineOption -WordDelimiters ' /\'
+@(
+    @{ Key = 'UpArrow'; Function = 'PreviousHistory' }
+    @{ Key = 'DownArrow'; Function = 'NextHistory' }
+    @{ Key = 'Tab', 'Shift+Tab'; Function = 'MenuComplete' }
+) | % { Set-PSReadLineKeyHandler @_ }
+
+Set-PSReadLineOption `
+    -EditMode Windows `
+    -PredictionViewStyle ListView `
+    -WordDelimiters ' /\' `
