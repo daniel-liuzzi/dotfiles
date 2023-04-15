32ee7f370dfd7ade2b3149fa8e4b6d972e6c987f
Daniel Liuzzi <dliuzzi@gmail.com>
2023-04-15 16:11:03 +0200

    Make archive a bit more resilient #ps

--- powershell/modules/archive.psm1
+++ powershell/modules/archive.psm1
@@ -2,8 +2,14 @@ function archive {
     # TODO: delete node_modules, bin, obj recursively
     $Date = Get-Date -Format 'yyyy-MM-dd'
     $Path = Join-Path '~/.archive' $Date
-    New-Item -Path $Path -ItemType Directory -Force | Out-Null
-    Get-ChildItem -Path '~/Desktop' -Recurse -Force | Move-Item -Destination $Path | Out-Null
+
+    New-Item -Path $Path -ItemType Directory -Force |
+    Out-Null
+
+    Get-ChildItem -Path '~/Desktop' -Recurse -Force |
+    Move-Item -Destination $Path -ErrorAction Ignore |
+    Out-Null
+
     Write-Output 'Desktop archived successfully.'
     Start-Process -FilePath (Resolve-Path $Path)
 }
