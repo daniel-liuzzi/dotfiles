12f67d939aaf1a25e92780d7015af86dc919a48c
Daniel Liuzzi <dliuzzi@gmail.com>
2023-11-23 16:05:19 +0100

    Make Get-FirefoxProfiles more resilient #ff

--- powershell/modules/firefox.psm1
+++ powershell/modules/firefox.psm1
@@ -3,11 +3,12 @@ using namespace System.IO
 Import-Module PsIni
 
 function Get-FirefoxProfiles {
-    $FirefoxPath = [Path]::Combine($env:APPDATA, 'Mozilla/Firefox')
+    $FirefoxPath = [Path]::Combine($env:APPDATA, 'Mozilla', 'Firefox')
     $ProfilesIniPath = [Path]::Combine($FirefoxPath, 'profiles.ini')
+    if (![Path]::Exists($ProfilesIniPath)) { return }
     $ProfilesIniContent = Get-IniContent $ProfilesIniPath
 
     $ProfilesIniContent.Keys |
-    Where-Object { $_.StartsWith('Profile') } |
-    ForEach-Object { [Path]::Combine($FirefoxPath, $ProfilesIniContent.$_.Path) }
+    where { $_.StartsWith('Profile') } |
+    foreach { [Path]::Combine($FirefoxPath, $ProfilesIniContent.$_.Path) }
 }
