551f6e6d2be00745cb585f67e6a77c1b8967584d
Daniel Liuzzi <dliuzzi@gmail.com>
2024-03-12 15:00:15 +0100

    Improve susus #ps

--- powershell/scoop.psm1
+++ powershell/scoop.psm1
@@ -1,9 +1,17 @@
 Import-Module $ProfileDir/base
 
 function susus {
-    run scoop status --local
+    function status {
+        scoop status --local | where Info -NE 'Held package'
+    }
+
+    status
     run scoop update
-    run scoop status --local
-    run scoop update --all
-    run scoop status --local
+    $status = status
+    $status
+
+    # we update one by one because scoop update --all stops upon first error
+    $status | foreach { run scoop update $_.Name }
+
+    status
 }
