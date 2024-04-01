a92539e1c610d4b4c38a31833f4f4c01e90f1ba3
Daniel Liuzzi <dliuzzi@gmail.com>
2024-04-01 21:13:21 +0200

    Update settings #ff
    - Don't allow Firefox to install or run studies
    - Don't allow Firefox to send technical or interaction data to Mozilla
    - Don't save or fill payment methods
    - Don't ask to save passwords

--- firefox/user.js
+++ firefox/user.js
@@ -1,3 +1,6 @@
+// Don't allow Firefox to install or run studies
+user_pref("app.shield.optoutstudies.enabled", false);
+
 // Don't warn when accessing about:config
 user_pref("browser.aboutConfig.showWarning", false);
 
@@ -12,6 +15,9 @@ user_pref("browser.toolbars.bookmarks.visibility", "never");
 
 // Never offer to translate
 user_pref("browser.translations.automaticallyPopup", false);
+
+// Don't allow Firefox to send technical or interaction data to Mozilla
+user_pref("datareporting.healthreport.uploadEnabled", false);
 
 // DevTools
 user_pref("devtools.browserconsole.contentMessages", true);
@@ -29,6 +35,9 @@ user_pref("devtools.webconsole.filter.netxhr", true);
 user_pref("devtools.webconsole.persistlog", true);
 user_pref("devtools.webconsole.timestampMessages", true);
 
+// Don't save or fill payment methods
+user_pref("extensions.formautofill.creditCards.enabled", false);
+
 // Allow Windows single sign-on for Microsoft, work, and school accounts
 // https://support.mozilla.org/en-US/kb/windows-sso
 user_pref("network.http.windows-sso.enabled", true);
@@ -36,6 +45,9 @@ user_pref("network.http.windows-sso.enabled", true);
 // Firefox gives certificate warning when accessing applications via Forcepoint ONE
 // https://support.forcepoint.com/s/article/Configure-Firefox-to-use-Windows-certificate-store-1648249639083
 user_pref("security.enterprise_roots.enabled", true);
+
+// Don't ask to save passwords
+user_pref("signon.rememberSignons", false);
 
 // Set Firefox to look for userChrome.css at startup
 // https://www.userchrome.org/how-create-userchrome-css.html#aboutconfig
