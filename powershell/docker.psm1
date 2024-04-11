3a0937dc809ee02779749d03c49b96bc7b1c3635
Daniel Liuzzi <dliuzzi@gmail.com>
2024-04-12 00:14:51 +0200

    Docker improvements #ps
    - Change `docker-compose` to `docker compose` [1]
    - Use functions instead of aliases
    
    [1]: https://docs.docker.com/compose/migrate/#docker-compose-vs-docker-compose

--- powershell/docker.psm1
+++ powershell/docker.psm1
@@ -1,13 +1,13 @@
 # Credit: https://stackoverflow.com/a/30793515/88709
 
-function Invoke-Whaler {
-    run docker run --rm -v /var/run/docker.sock:/var/run/docker.sock alpine/dfimage $args
+function dc {
+    run docker compose @args
 }
 
-function Invoke-Dive {
-    run docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock wagoodman/dive $args
+function dive {
+    run docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock wagoodman/dive @args
 }
 
-Set-Alias -Name 'whaler' -Value 'Invoke-Whaler'
-Set-Alias -Name 'dive' -Value 'Invoke-Dive'
-Set-Alias -Name 'dc' -Value 'docker-compose'
+function whaler {
+    run docker run --rm -v /var/run/docker.sock:/var/run/docker.sock alpine/dfimage @args
+}
