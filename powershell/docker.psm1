183a72aeadf1b975eebcccf0086f00ff2dfd21a6
Daniel Liuzzi <dliuzzi@gmail.com>
2024-04-28 08:07:53 +0200

    Handy Docker functions #ps

--- powershell/docker.psm1
+++ powershell/docker.psm1
@@ -1,13 +1,33 @@
-# Credit: https://stackoverflow.com/a/30793515/88709
+function d_ { run docker @args }
+function dc { d_ compose @args }
 
-function dc {
-    run docker compose @args
-}
+# docker run
+function dr { d_ run @args }
+function drd { dr --detach @args }
+function drdrm { drd --rm @args }
+function dri { dr --interactive @args }
+function drirm { dri --rm @args }
+function drit { dri --tty @args }
+function dritrm { drit --rm @args }
+function drrm { dr --rm @args }
 
-function dive {
-    run docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock wagoodman/dive @args
-}
+# docker ps
+function dps { d_ ps @args }
+function dpsa { dps --all @args }
 
-function whaler {
-    run docker run --rm -v /var/run/docker.sock:/var/run/docker.sock alpine/dfimage @args
-}
+# docker compose up
+function dcu { dc up @args }
+function dcud { dcu --detach @args }
+function dcudbfr { dcud --build --force-recreate @args }
+
+# docker compose down
+function dcd { dc down @args }
+function dcdv { dcd --volumes @args }
+
+# docker compose ps
+function dcps { dc ps @args }
+function dcpsa { dcps --all @args }
+
+# misc. - credit: https://stackoverflow.com/a/30793515/88709
+function dfimage { drrm --volume /var/run/docker.sock:/var/run/docker.sock alpine/dfimage @args }
+function dive { dritrm --volume /var/run/docker.sock:/var/run/docker.sock wagoodman/dive @args }
