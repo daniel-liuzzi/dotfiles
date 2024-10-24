627458de0471d4253bf79c3d54577c8a28022e62
Daniel Liuzzi <dliuzzi@gmail.com>
2024-10-24 12:45:51 +0200

    Improve o/open #ps

--- powershell/misc.psm1
+++ powershell/misc.psm1
@@ -12,6 +12,10 @@ function ......... { Set-Location '../../../../../../../..' }
 function .......... { Set-Location '../../../../../../../../..' }
 function ........... { Set-Location '../../../../../../../../../..' }
 
+function o. { o . @args }
+function o { open @args }
+function open ([string]$FilePath = '.') { run start $FilePath @args }
+
 function Get-SavedHistory { Get-Content (Get-PSReadlineOption).HistorySavePath @args }
 function hosts { sudo code $env:SystemRoot\System32\drivers\etc\hosts }
 function la { Get-ChildItem -Force @args }
@@ -22,6 +26,4 @@ function which { Get-Command @args | Select-Object -ExpandProperty Definition }
 Set-Alias -Name 'ads' -Value 'azuredatastudio'
 Set-Alias -Name 'hist' -Value 'Get-SavedHistory'
 Set-Alias -Name 'LogParser' -Value "${env:ProgramFiles(x86)}/Log Parser 2.2/LogParser.exe"
-Set-Alias -Name 'o' -Value 'Start-Process'
-Set-Alias -Name 'open' -Value 'Start-Process'
 Set-Alias -Name 'top' -Value 'ntop'
